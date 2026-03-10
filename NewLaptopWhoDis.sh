696d5a18c653709262560d9003d40538112afa3a
tillay <tilleyy8@gmail.com>
2026-03-10 15:46:49 -0600

    fix typo

--- NewLaptopWhoDis.sh
+++ NewLaptopWhoDis.sh
@@ -1,4 +1,4 @@
-interface = $(ip -o link show up | awk -F': ' '/BROADCAST/ {print $2}')
+interface=$(ip -o link show up | awk -F': ' '/BROADCAST/ {print $2}')
 sudo ip link set dev $interface down
 sudo hostnamectl set-hostname $(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c8) --static
 sudo macchanger -r $interface 
